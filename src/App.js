abdcd39e06196ed6b3559f73d76415ba22ddaee1
Sawant-Raj <2019kuec2028@iiitkota.ac.in>
2024-03-24 13:52:30 +0530

    adding a modal via a React Portal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
 import Meals from ".//components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
   return (
     <Fragment>
+      {/* exact position of Cart does not matter too much since we use a modal with a portal */}
+      <Cart />
       <Header />
       <main>
-      <Meals/>
+        <Meals />
       </main>
     </Fragment>
   );
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,34 @@
+import { Fragment } from "react";
+import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+        
+      {/* <Backdrop />
+      <ModalOverlay>{props.children}</ModalOverlay> could have done it like this if we were not using createPortal and it is not necessary to use createPortal, we are using it to ensure that the HTML code is not all over the place in the final app */}
+
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+export default Modal;
