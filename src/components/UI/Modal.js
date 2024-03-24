c7d64f3ebc9f889d7cb6a25d3c010305b3bd78a3
Sawant-Raj <2019kuec2028@iiitkota.ac.in>
2024-03-24 15:01:02 +0530

    cart and modal state

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,13 @@ import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return (
+    <div
+      className={classes.backdrop}
+      onClick={props.onClose}
+      // onClick={props.onCloseCartBackdrop}
+    ></div>
+  );
 };
 
 const ModalOverlay = (props) => {
@@ -19,11 +25,17 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-        
+      
       {/* <Backdrop />
       <ModalOverlay>{props.children}</ModalOverlay> could have done it like this if we were not using createPortal and it is not necessary to use createPortal, we are using it to ensure that the HTML code is not all over the place in the final app */}
 
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <Backdrop
+          onClose={props.onClose}
+          // onCloseCartBackdrop={props.onCloseCart}
+        />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
